bd1273dcff1ef11acac16381ac39764ddc400b5e
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-09-11 23:32:11 +0300

    2.63

--- server/src/test/java/ru/practicum/shareit/booking/mapping/MappingBookingTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/mapping/MappingBookingTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasProperty;
 
+
 public class MappingBookingTest {
 
     private final LocalDateTime start = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
