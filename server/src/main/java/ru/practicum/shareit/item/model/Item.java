5cb68e7e2badf6d1ebbf44c8f088c763c667f733
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-09-11 23:19:26 +0300

    2.61

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.util.ArrayList;
 import java.util.List;
